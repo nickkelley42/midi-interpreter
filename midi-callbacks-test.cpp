a2618a487733ce9176fbcee429b349adab96bde5
Nick Kelley <nick@nickkelley.dev>
2021-11-02 18:51:09 -0400

    Add demos of more event types

--- midi-callbacks-test.cpp
+++ midi-callbacks-test.cpp
@@ -5,8 +5,27 @@ void printPitch(uint16_t p) {
 	std::cout << "Pitch:\t" << static_cast<unsigned int>(p) << std::endl;
 }
 
+void printChannelPressure(uint8_t p) {
+	std::cout << "CPressure:\t" << static_cast<unsigned int>(p) << std::endl;
+}
+
+void printPolyPressure(uint8_t a, uint8_t b) {
+	std::cout << "PolyKeyPressure:\t" << static_cast<unsigned int>(a)
+		<< static_cast<unsigned int>(b) << std::endl;
+}
+
 void printCC(uint8_t a, uint8_t b) {
 	std::cout << "CC:\t" << static_cast<unsigned int>(a)
+		<< "\t" << static_cast<unsigned int>(b) << std::endl;
+}
+
+void printNoteOn(uint8_t a, uint8_t b) {
+	std::cout << "Note On:\t" << static_cast<unsigned int>(a)
+		<< "\t" << static_cast<unsigned int>(b) << std::endl;
+}
+
+void printNoteOff(uint8_t a, uint8_t b) {
+	std::cout << "Note Off:\t" << static_cast<unsigned int>(a)
 		<< "\t" << static_cast<unsigned int>(b) << std::endl;
 }
 
@@ -15,6 +34,10 @@ int main() {
 
 	ms.setCallback(Dyadic7Message::ControlChange, printCC);
 	ms.setCallback(Monadic14Message::PitchBend, printPitch);
+	ms.setCallback(Dyadic7Message::NoteOn, printNoteOn);
+	ms.setCallback(Dyadic7Message::NoteOff, printNoteOff);
+	ms.setCallback(Dyadic7Message::PolyKeyPressure, printPolyPressure);
+	ms.setCallback(Monadic7Message::ChannelPressure, printChannelPressure);
 
 	while (ms.hasNext()) {
 		ms.next();
